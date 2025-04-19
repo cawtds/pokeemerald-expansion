3d3dc036396ac22f832bf34bcde956ba036576fe
cawtds <38510667+cawtds@users.noreply.github.com>
2025-04-19 17:29:09 +0200

    pokemon.h constants to enum

--- include/constants/pokemon.h
+++ include/constants/pokemon.h
@@ -57,19 +57,24 @@ enum Nature
 };
 
 // Pokémon Stats
-#define STAT_HP      0
-#define STAT_ATK     1
-#define STAT_DEF     2
-#define STAT_SPEED   3
-#define STAT_SPATK   4
-#define STAT_SPDEF   5
-#define NUM_STATS    6
+enum Stat
+{
+    STAT_HP,
+    STAT_ATK,
+    STAT_DEF,
+    STAT_SPEED,
+    STAT_SPATK,
+    STAT_SPDEF,
 
-#define STAT_ACC     6 // Only in battles.
-#define STAT_EVASION 7 // Only in battles.
+    NUM_STATS,
+    
+    STAT_ACC = NUM_STATS,   // Only in battles.
+    STAT_EVASION,           // Only in battles.
+
+    NUM_BATTLE_STATS        // includes Accuracy and Evasion
+};
 
 #define NUM_NATURE_STATS (NUM_STATS - 1) // excludes HP
-#define NUM_BATTLE_STATS (NUM_STATS + 2) // includes Accuracy and Evasion
 
 #define MIN_STAT_STAGE     0
 #define DEFAULT_STAT_STAGE 6
@@ -160,24 +165,30 @@ enum Ribbon
 #define MON_GENDERLESS 0xFF
 
 // Constants for AdjustFriendship
-#define FRIENDSHIP_EVENT_GROW_LEVEL       0
-#define FRIENDSHIP_EVENT_VITAMIN          1 // unused, handled by PokemonUseItemEffects
-#define FRIENDSHIP_EVENT_BATTLE_ITEM      2 // unused, handled by PokemonUseItemEffects
-#define FRIENDSHIP_EVENT_LEAGUE_BATTLE    3
-#define FRIENDSHIP_EVENT_LEARN_TMHM       4
-#define FRIENDSHIP_EVENT_WALKING          5
-#define FRIENDSHIP_EVENT_FAINT_SMALL      6
-#define FRIENDSHIP_EVENT_FAINT_FIELD_PSN  7
-#define FRIENDSHIP_EVENT_FAINT_LARGE      8 // If opponent was >= 30 levels higher. See AdjustFriendshipOnBattleFaint
+enum FriendshipEvent
+{
+    FRIENDSHIP_EVENT_GROW_LEVEL,
+    FRIENDSHIP_EVENT_VITAMIN,           // unused, handled by PokemonUseItemEffects
+    FRIENDSHIP_EVENT_BATTLE_ITEM,       // unused, handled by PokemonUseItemEffects
+    FRIENDSHIP_EVENT_LEAGUE_BATTLE,
+    FRIENDSHIP_EVENT_LEARN_TMHM,
+    FRIENDSHIP_EVENT_WALKING,
+    FRIENDSHIP_EVENT_FAINT_SMALL,
+    FRIENDSHIP_EVENT_FAINT_FIELD_PSN,
+    FRIENDSHIP_EVENT_FAINT_LARGE,       // If opponent was >= 30 levels higher. See AdjustFriendshipOnBattleFaint
+};
 
 // Constants for GetLeadMonFriendshipScore
-#define FRIENDSHIP_NONE        0
-#define FRIENDSHIP_1_TO_49     1
-#define FRIENDSHIP_50_TO_99    2
-#define FRIENDSHIP_100_TO_149  3
-#define FRIENDSHIP_150_TO_199  4
-#define FRIENDSHIP_200_TO_254  5
-#define FRIENDSHIP_MAX         6
+enum FriendshipScore
+{
+    FRIENDSHIP_NONE,
+    FRIENDSHIP_1_TO_49,
+    FRIENDSHIP_50_TO_99,
+    FRIENDSHIP_100_TO_149,
+    FRIENDSHIP_150_TO_199,
+    FRIENDSHIP_200_TO_254,
+    FRIENDSHIP_MAX,
+};
 
 // Friendship value that the majority of species use. This was changed in Generation 8 to 50.
 #define STANDARD_FRIENDSHIP 70
@@ -194,51 +205,64 @@ enum Ribbon
 #define EV_ITEM_RAISE_LIMIT 100
 
 // Growth rates
-#define GROWTH_MEDIUM_FAST  0
-#define GROWTH_ERRATIC      1
-#define GROWTH_FLUCTUATING  2
-#define GROWTH_MEDIUM_SLOW  3
-#define GROWTH_FAST         4
-#define GROWTH_SLOW         5
+enum GrowthRate
+{
+    GROWTH_MEDIUM_FAST,
+    GROWTH_ERRATIC,
+    GROWTH_FLUCTUATING,
+    GROWTH_MEDIUM_SLOW,
+    GROWTH_FAST,
+    GROWTH_SLOW,
+};
 
 // Body colors for Pokédex search
-#define BODY_COLOR_RED      0
-#define BODY_COLOR_BLUE     1
-#define BODY_COLOR_YELLOW   2
-#define BODY_COLOR_GREEN    3
-#define BODY_COLOR_BLACK    4
-#define BODY_COLOR_BROWN    5
-#define BODY_COLOR_PURPLE   6
-#define BODY_COLOR_GRAY     7
-#define BODY_COLOR_WHITE    8
-#define BODY_COLOR_PINK     9
+enum BodyColor
+{
+    BODY_COLOR_RED,
+    BODY_COLOR_BLUE,
+    BODY_COLOR_YELLOW,
+    BODY_COLOR_GREEN,
+    BODY_COLOR_BLACK,
+    BODY_COLOR_BROWN,
+    BODY_COLOR_PURPLE,
+    BODY_COLOR_GRAY,
+    BODY_COLOR_WHITE,
+    BODY_COLOR_PINK,
+};
 
 #define F_SUMMARY_SCREEN_FLIP_SPRITE 0x80
 
 // Evolution types
-#define EVO_FRIENDSHIP       1  // Pokémon levels up with friendship ≥ 220
-#define EVO_FRIENDSHIP_DAY   2  // Pokémon levels up during the day with friendship ≥ 220
-#define EVO_FRIENDSHIP_NIGHT 3  // Pokémon levels up at night with friendship ≥ 220
-#define EVO_LEVEL            4  // Pokémon reaches the specified level
-#define EVO_TRADE            5  // Pokémon is traded
-#define EVO_TRADE_ITEM       6  // Pokémon is traded while it's holding the specified item
-#define EVO_ITEM             7  // specified item is used on Pokémon
-#define EVO_LEVEL_ATK_GT_DEF 8  // Pokémon reaches the specified level with attack > defense
-#define EVO_LEVEL_ATK_EQ_DEF 9  // Pokémon reaches the specified level with attack = defense
-#define EVO_LEVEL_ATK_LT_DEF 10 // Pokémon reaches the specified level with attack < defense
-#define EVO_LEVEL_SILCOON    11 // Pokémon reaches the specified level with a Silcoon personality value
-#define EVO_LEVEL_CASCOON    12 // Pokémon reaches the specified level with a Cascoon personality value
-#define EVO_LEVEL_NINJASK    13 // Pokémon reaches the specified level (special value for Ninjask)
-#define EVO_LEVEL_SHEDINJA   14 // Pokémon reaches the specified level (special value for Shedinja)
-#define EVO_BEAUTY           15 // Pokémon levels up with beauty ≥ specified value
+enum EvolutionType
+{
+    EVO_NONE,
+    EVO_FRIENDSHIP,         // Pokémon levels up with friendship ≥ 220
+    EVO_FRIENDSHIP_DAY,     // Pokémon levels up during the day with friendship ≥ 220
+    EVO_FRIENDSHIP_NIGHT,   // Pokémon levels up at night with friendship ≥ 220
+    EVO_LEVEL,              // Pokémon reaches the specified level
+    EVO_TRADE,              // Pokémon is traded
+    EVO_TRADE_ITEM,         // Pokémon is traded while it's holding the specified item
+    EVO_ITEM,               // specified item is used on Pokémon
+    EVO_LEVEL_ATK_GT_DEF,   // Pokémon reaches the specified level with attack > defense
+    EVO_LEVEL_ATK_EQ_DEF,   // Pokémon reaches the specified level with attack = defense
+    EVO_LEVEL_ATK_LT_DEF,   // Pokémon reaches the specified level with attack < defense
+    EVO_LEVEL_SILCOON,      // Pokémon reaches the specified level with a Silcoon personality value
+    EVO_LEVEL_CASCOON,      // Pokémon reaches the specified level with a Cascoon personality value
+    EVO_LEVEL_NINJASK,      // Pokémon reaches the specified level (special value for Ninjask)
+    EVO_LEVEL_SHEDINJA,     // Pokémon reaches the specified level (special value for Shedinja)
+    EVO_BEAUTY,             // Pokémon levels up with beauty ≥ specified value
+};
 
 #define EVOS_PER_MON 5
 
 // Evolution 'modes,' for GetEvolutionTargetSpecies
-#define EVO_MODE_NORMAL     0
-#define EVO_MODE_TRADE      1
-#define EVO_MODE_ITEM_USE   2
-#define EVO_MODE_ITEM_CHECK 3 // If an Everstone is being held, still want to show that the stone *could* be used on that Pokémon to evolve
+enum EvolutionMode
+{
+    EVO_MODE_NORMAL,
+    EVO_MODE_TRADE,
+    EVO_MODE_ITEM_USE,
+    EVO_MODE_ITEM_CHECK,    // If an Everstone is being held, still want to show that the stone *could* be used on that Pokémon to evolve
+};
 
 #define MON_PIC_WIDTH 64
 #define MON_PIC_HEIGHT 64
@@ -260,11 +284,200 @@ enum Ribbon
 
 #define NUM_ABILITY_SLOTS 2
 
-enum {
+enum PokemonJumpType
+{
     PKMN_JUMP_TYPE_NONE,
     PKMN_JUMP_TYPE_NORMAL,
     PKMN_JUMP_TYPE_FAST,
     PKMN_JUMP_TYPE_SLOW,
+};
+
+// Pokémon animation function ids (for front and back)
+// Each front anim uses 1, and each back anim uses a set of 3
+enum Animation
+{
+    ANIM_V_SQUISH_AND_BOUNCE,
+    ANIM_CIRCULAR_STRETCH_TWICE,
+    ANIM_H_VIBRATE,
+    ANIM_H_SLIDE,
+    ANIM_V_SLIDE,
+    ANIM_BOUNCE_ROTATE_TO_SIDES,
+    ANIM_V_JUMPS_H_JUMPS,
+    ANIM_ROTATE_TO_SIDES,
+    ANIM_ROTATE_TO_SIDES_TWICE,
+    ANIM_GROW_VIBRATE,
+    ANIM_ZIGZAG_FAST,
+    ANIM_SWING_CONCAVE,
+    ANIM_SWING_CONCAVE_FAST,
+    ANIM_SWING_CONVEX,
+    ANIM_SWING_CONVEX_FAST,
+    ANIM_H_SHAKE,
+    ANIM_V_SHAKE,
+    ANIM_CIRCULAR_VIBRATE,
+    ANIM_TWIST,
+    ANIM_SHRINK_GROW,
+    ANIM_CIRCLE_C_CLOCKWISE,
+    ANIM_GLOW_BLACK,
+    ANIM_H_STRETCH,
+    ANIM_V_STRETCH,
+    ANIM_RISING_WOBBLE,
+    ANIM_V_SHAKE_TWICE,
+    ANIM_TIP_MOVE_FORWARD,
+    ANIM_H_PIVOT,
+    ANIM_V_SLIDE_WOBBLE,
+    ANIM_H_SLIDE_WOBBLE,
+    ANIM_V_JUMPS_BIG,
+    ANIM_SPIN_LONG,
+    ANIM_GLOW_ORANGE,
+    ANIM_GLOW_RED,
+    ANIM_GLOW_BLUE,
+    ANIM_GLOW_YELLOW,
+    ANIM_GLOW_PURPLE,
+    ANIM_BACK_AND_LUNGE,
+    ANIM_BACK_FLIP,
+    ANIM_FLICKER,
+    ANIM_BACK_FLIP_BIG,
+    ANIM_FRONT_FLIP,
+    ANIM_TUMBLING_FRONT_FLIP,
+    ANIM_FIGURE_8,
+    ANIM_FLASH_YELLOW,
+    ANIM_SWING_CONCAVE_FAST_SHORT,
+    ANIM_SWING_CONVEX_FAST_SHORT,
+    ANIM_ROTATE_UP_SLAM_DOWN,
+    ANIM_DEEP_V_SQUISH_AND_BOUNCE,
+    ANIM_H_JUMPS,
+    ANIM_H_JUMPS_V_STRETCH,
+    ANIM_ROTATE_TO_SIDES_FAST,
+    ANIM_ROTATE_UP_TO_SIDES,
+    ANIM_FLICKER_INCREASING,
+    ANIM_TIP_HOP_FORWARD,
+    ANIM_PIVOT_SHAKE,
+    ANIM_TIP_AND_SHAKE,
+    ANIM_VIBRATE_TO_CORNERS,
+    ANIM_GROW_IN_STAGES,
+    ANIM_V_SPRING,
+    ANIM_V_REPEATED_SPRING,
+    ANIM_SPRING_RISING,
+    ANIM_H_SPRING,
+    ANIM_H_REPEATED_SPRING_SLOW,
+    ANIM_H_SLIDE_SHRINK,
+    ANIM_LUNGE_GROW,
+    ANIM_CIRCLE_INTO_BG,
+    ANIM_RAPID_H_HOPS,
+    ANIM_FOUR_PETAL,
+    ANIM_V_SQUISH_AND_BOUNCE_SLOW,
+    ANIM_H_SLIDE_SLOW,
+    ANIM_V_SLIDE_SLOW,
+    ANIM_BOUNCE_ROTATE_TO_SIDES_SMALL,
+    ANIM_BOUNCE_ROTATE_TO_SIDES_SLOW,
+    ANIM_BOUNCE_ROTATE_TO_SIDES_SMALL_SLOW,
+    ANIM_ZIGZAG_SLOW,
+    ANIM_H_SHAKE_SLOW,
+    ANIM_V_SHAKE_SLOW,
+    ANIM_TWIST_TWICE,
+    ANIM_CIRCLE_C_CLOCKWISE_SLOW,
+    ANIM_V_SHAKE_TWICE_SLOW,
+    ANIM_V_SLIDE_WOBBLE_SMALL,
+    ANIM_V_JUMPS_SMALL,
+    ANIM_SPIN,
+    ANIM_TUMBLING_FRONT_FLIP_TWICE,
+    ANIM_DEEP_V_SQUISH_AND_BOUNCE_TWICE,
+    ANIM_H_JUMPS_V_STRETCH_TWICE,
+    ANIM_V_SHAKE_BACK,
+    ANIM_V_SHAKE_BACK_SLOW,
+    ANIM_V_SHAKE_H_SLIDE_SLOW,
+    ANIM_V_STRETCH_BOTH_ENDS_SLOW,
+    ANIM_H_STRETCH_FAR_SLOW,
+    ANIM_V_SHAKE_LOW_TWICE,
+    ANIM_H_SHAKE_FAST,
+    ANIM_H_SLIDE_FAST,
+    ANIM_H_VIBRATE_FAST,
+    ANIM_H_VIBRATE_FASTEST,
+    ANIM_V_SHAKE_BACK_FAST,
+    ANIM_V_SHAKE_LOW_TWICE_SLOW,
+    ANIM_V_SHAKE_LOW_TWICE_FAST,
+    ANIM_CIRCLE_C_CLOCKWISE_LONG,
+    ANIM_GROW_STUTTER_SLOW,
+    ANIM_V_SHAKE_H_SLIDE,
+    ANIM_V_SHAKE_H_SLIDE_FAST,
+    ANIM_TRIANGLE_DOWN_SLOW,
+    ANIM_TRIANGLE_DOWN,
+    ANIM_TRIANGLE_DOWN_TWICE,
+    ANIM_GROW,
+    ANIM_GROW_TWICE,
+    ANIM_H_SPRING_FAST,
+    ANIM_H_SPRING_SLOW,
+    ANIM_H_REPEATED_SPRING_FAST,
+    ANIM_H_REPEATED_SPRING,
+    ANIM_SHRINK_GROW_FAST,
+    ANIM_SHRINK_GROW_SLOW,
+    ANIM_V_STRETCH_BOTH_ENDS,
+    ANIM_V_STRETCH_BOTH_ENDS_TWICE,
+    ANIM_H_STRETCH_FAR_TWICE,
+    ANIM_H_STRETCH_FAR,
+    ANIM_GROW_STUTTER_TWICE,
+    ANIM_GROW_STUTTER,
+    ANIM_CONCAVE_ARC_LARGE_SLOW,
+    ANIM_CONCAVE_ARC_LARGE,
+    ANIM_CONCAVE_ARC_LARGE_TWICE,
+    ANIM_CONVEX_DOUBLE_ARC_SLOW,
+    ANIM_CONVEX_DOUBLE_ARC,
+    ANIM_CONVEX_DOUBLE_ARC_TWICE,
+    ANIM_CONCAVE_ARC_SMALL_SLOW,
+    ANIM_CONCAVE_ARC_SMALL,
+    ANIM_CONCAVE_ARC_SMALL_TWICE,
+    ANIM_H_DIP,
+    ANIM_H_DIP_FAST,
+    ANIM_H_DIP_TWICE,
+    ANIM_SHRINK_GROW_VIBRATE_FAST,
+    ANIM_SHRINK_GROW_VIBRATE,
+    ANIM_SHRINK_GROW_VIBRATE_SLOW,
+    ANIM_JOLT_RIGHT_FAST,
+    ANIM_JOLT_RIGHT,
+    ANIM_JOLT_RIGHT_SLOW,
+    ANIM_SHAKE_FLASH_YELLOW_FAST,
+    ANIM_SHAKE_FLASH_YELLOW,
+    ANIM_SHAKE_FLASH_YELLOW_SLOW,
+    ANIM_SHAKE_GLOW_RED_FAST,
+    ANIM_SHAKE_GLOW_RED,
+    ANIM_SHAKE_GLOW_RED_SLOW,
+    ANIM_SHAKE_GLOW_GREEN_FAST,
+    ANIM_SHAKE_GLOW_GREEN,
+    ANIM_SHAKE_GLOW_GREEN_SLOW,
+    ANIM_SHAKE_GLOW_BLUE_FAST,
+    ANIM_SHAKE_GLOW_BLUE,
+    ANIM_SHAKE_GLOW_BLUE_SLOW,
+};
+
+// Pokémon back animation sets
+enum BackAnimation
+{
+    BACK_ANIM_NONE,
+    BACK_ANIM_H_VIBRATE,
+    BACK_ANIM_H_SLIDE,
+    BACK_ANIM_H_SPRING,
+    BACK_ANIM_H_SPRING_REPEATED,
+    BACK_ANIM_SHRINK_GROW,
+    BACK_ANIM_GROW,
+    BACK_ANIM_CIRCLE_COUNTERCLOCKWISE,
+    BACK_ANIM_H_SHAKE,
+    BACK_ANIM_V_SHAKE,
+    BACK_ANIM_V_SHAKE_H_SLIDE,
+    BACK_ANIM_V_STRETCH,
+    BACK_ANIM_H_STRETCH,
+    BACK_ANIM_GROW_STUTTER,
+    BACK_ANIM_V_SHAKE_LOW,
+    BACK_ANIM_TRIANGLE_DOWN,
+    BACK_ANIM_CONCAVE_ARC_LARGE,
+    BACK_ANIM_CONVEX_DOUBLE_ARC,
+    BACK_ANIM_CONCAVE_ARC_SMALL,
+    BACK_ANIM_DIP_RIGHT_SIDE,
+    BACK_ANIM_SHRINK_GROW_VIBRATE,
+    BACK_ANIM_JOLT_RIGHT,
+    BACK_ANIM_SHAKE_FLASH_YELLOW,
+    BACK_ANIM_SHAKE_GLOW_RED,
+    BACK_ANIM_SHAKE_GLOW_GREEN,
+    BACK_ANIM_SHAKE_GLOW_BLUE,
 };
 
 #endif // GUARD_CONSTANTS_POKEMON_H
